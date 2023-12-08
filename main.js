57a9d31ce43207348d6a5f1c700edd261e2e2f5d
devadirup <137271917+devadirup@users.noreply.github.com>
2023-12-08 14:33:39 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-
+var typed= new Typed(".text", {
+    strings: [" Frontend Development" , "Logo Designer" , "Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
