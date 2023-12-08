147102eb1b68072f4cba742918bfe311a37253df
devadirup <137271917+devadirup@users.noreply.github.com>
2023-12-08 14:15:04 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new typed(".text",{
+    Strings:["Frontend Develpment" , "Logo Designer" , "Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
